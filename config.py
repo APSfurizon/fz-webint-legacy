b15f92fb7af364588cf3f0000385ad08a2cf37c1
Ed <ed@404.blue>
2022-12-19 18:59:19 +0100

    Added local API endpoint

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-headers = {'Authorization': 'Token 2q5h7a2z9tqfz8ogu0qpriclwufelswc7obapw1mq2l1b4zaos7ebpz424zvz5gv'}
-base_url = "https://reg.furizon.net/api/v1/organizers/furizon/events/beyond/"
+headers = {'Host': 'reg.furizon.net', 'Authorization': 'Token 2q5h7a2z9tqfz8ogu0qpriclwufelswc7obapw1mq2l1b4zaos7ebpz424zvz5gv'}
+base_url = "http://127.0.0.1:8345/api/v1/organizers/furizon/events/beyond/"
