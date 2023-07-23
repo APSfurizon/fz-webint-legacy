b1fb2bb1058924b249f49a989548e73298b9f21d
Ed <me@dummy.mail>
2023-07-23 22:34:03 +0200

    Added config example

---
+++ config.example.py
@@ -0,0 +1,11 @@
+headers = {'Host': 'your-pretix-hostname', 'Authorization': 'Token XXXXXXXXXXXXXXXXXXXXXXXX'}
+base_url = "https://your-pretix-hostname/api/v1/organizers/organizer-name/events/your-event/"
+
+PROPIC_DEADLINE = 1683575684
+FILL_CACHE = True
+
+DEV_MODE = True
+
+SMTP_HOST = 'your-smtp-host.com'
+SMTP_USER = 'username'
+SMTP_PASSWORD = 'password'
--- config.py
+++
@@ -1,2 +0,0 @@
-headers = {'Host': 'reg.furizon.net', 'Authorization': 'Token 2q5h7a2z9tqfz8ogu0qpriclwufelswc7obapw1mq2l1b4zaos7ebpz424zvz5gv'}
-base_url = "http://127.0.0.1:8345/api/v1/organizers/furizon/events/beyond/"
