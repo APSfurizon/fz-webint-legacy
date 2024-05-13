63c0bb75db4893c9d9182679ffe48138f136e0c8
Andrea <rctandrew100@gmail.com>
2024-05-14 00:25:43 +0200

    room autofill wip
    Added a button in the nosecount
    [Might be removed] a new dialog

--- res/scripts/roomManager.js
+++ res/scripts/roomManager.js
@@ -1,11 +1,11 @@
 function confirmAction (intent, sender) {
     if (['rename', 'unconfirm', 'delete'].includes (intent) == false) return
     let href = sender.getAttribute('action')
-    let intentTitle = document.querySelector("#intentText")
-    let intentEdit =  document.querySelector("#intentRename")
-    let intentEditPanel =  document.querySelector("#intentEditPanel")
+    let intentTitle = document.querySelector("#modalOrderEditDialog #intentText")
+    let intentEdit =  document.querySelector("#modalOrderEditDialog #intentRename")
+    let intentEditPanel =  document.querySelector("#modalOrderEditDialog #intentEditPanel")
     let intentFormAction =  document.querySelector("#intentFormAction")
-    let intentSend =  document.querySelector("#intentSend")
+    let intentSend =  document.querySelector("#modalOrderEditDialog #intentSend")
     // Resetting ui
     intentEdit.removeAttribute('required')
     intentEdit.removeAttribute('minlength')
@@ -27,5 +27,29 @@ function confirmAction (intent, sender) {
         case 'delete':
             break
     }
-    document.getElementById('modalRoomconfirm').setAttribute('open', 'true');
+    document.getElementById('modalOrderEditDialog').setAttribute('open', 'true');
+}
+
+function setLoading (){
+    document.getElementById('loadingIndicator').style.visibility = 'unset';
+}
+
+function setLoaded (){
+    document.getElementById('loadingIndicator').style.visibility = 'hidden';
+}
+
+async function roomGuestEditAction (intent, sender) {
+    if (['new', 'fromOrder'].includes (intent) == false) return
+    setLoaded ();
+    document.getElementById('roomGuestEditorDialog').setAttribute('open', 'true');
+}
+
+async function retrieveUsers (type) {
+    setLoading();
+
+    setLoaded();
+}
+
+function adaptRoomFormat (){
+    
 }
